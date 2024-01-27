bb4bd93e8c16773c800f5be2138800c57ee644a7
shamim496 <mdshamimhasan496@gmail.com>
2024-01-27 12:25:50 +0600

    type of

--- js/script.js
+++ js/script.js
@@ -866,3 +866,45 @@
 // and 2 data types that cannot contain values
 // null, undefined
 
+// typeof "John"                 // Returns "string"
+// typeof 3.14                   // Returns "number"
+// typeof NaN                    // Returns "number"
+// typeof false                  // Returns "boolean"
+// typeof [1, 2, 3, 4]              // Returns "object"
+// typeof { name: 'John', age: 34 }  // Returns "object"
+// typeof new Date()             // Returns "object"
+// typeof function () { }         // Returns "function"
+// typeof myCar                  // Returns "undefined" *
+// typeof null
+
+// typeof mycar;
+// console.log(mycar)
+
+// primitive Data
+// String, number, Boolean, undefined
+
+// Complex Data
+// function, Object
+
+// The constructor property
+
+// const name = "john";
+// console.log(name.constructor);
+
+// function isArray(myArray) {
+//     console.log(myArray.constructor.toString().indexOf("Array") > -1);
+// }
+// isArray({fname: "shamim"});
+
+// function isArray(myArray) {
+//  return myArray.constructor === Array;
+// }
+// console.log(isArray([1, 2, 3]));
+
+let person = {
+    fname: "shamim",
+    lname: "hasan",
+    age: "20",
+    eyecolor: "black"
+};
+console.log(person.null);
