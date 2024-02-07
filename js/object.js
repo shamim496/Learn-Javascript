1f7b512ebf66ad094e226ca7748c086a71588df7
shamim496 <mdshamimhasan496@gmail.com>
2024-02-07 13:27:38 +0600

    get keywords

--- js/object.js
+++ js/object.js
@@ -89,6 +89,149 @@
 
 // prototype properties
 
-// let a = "Bangladesh";
+// let a = new String ("Bangladesh");
 
 // console.dir(a);
+
+
+
+// 3 *** Javascript object Method
+
+// const person = {
+//     firstName: "Shamim",
+//     lastName: "Hasan",
+//     fullName: function () {
+//         return this.firstName + " " + this.lastName;
+//     };
+// }
+
+// console.log(person.fullName());
+
+// const person = {
+//     firstName: "Shamim",
+//     lastName: "Hasan",
+//     id: 4968,
+// };
+
+// // Adding a method to an object
+// person.fullName = function () {
+//     return this.firstName + " " + this.lastName;
+// }
+
+// console.log(person.fullName());
+
+// using Bulit in methods
+
+// const person = {
+//     fname: "shamim",
+//     lname: "hasan",
+//     id: 4968
+// };
+
+// person.fullName = function () {
+//     return (this.fname + " " + this.lname).toUpperCase();
+// }
+
+// console.log(person.fullName());
+
+
+
+// 4 *** Javascript object Display ***
+
+// How to Display Javascript Objects [object object]
+
+// const person = {
+//     fname: "Shamim",
+//     lname: "Hasan",
+//     city: "Bogura"
+// };
+
+// document.getElementById("demo").innerHTML = person.fname + ' ' + person.city;
+
+// const person = {
+//     name: "Shamim",
+//     age: 30,
+//     city: "Bogura"
+// };
+
+// let all = "";
+
+// for (let x in person) {
+//     all += person[x];
+// };
+
+// document.getElementById("demo").innerHTML = all;
+
+
+// using Object.values()
+
+// const person = {
+//     name: "Shamim",
+//     age:  30,
+//     city: "Bogura"
+// }
+
+// const myArray = Object.values(person);
+
+// Object loop for of
+// for (let x of myArray) {
+//     console.log(x);
+// }
+
+
+// using JSON.stringify()
+
+// const person = {
+//     name: "Shamim",
+//     age: 30,
+//     city: "Bogura"
+// };
+
+// console.log(JSON.stringify(person));
+
+// strigify date
+
+// const person = {
+//     name: "shamim",
+//     today: new Date()
+// };
+
+// let myString = JSON.stringify(person);
+// console.log(myString);
+
+// strigify function
+
+// const person = {
+//     name: "John",
+//     age: function () { return 30; }
+// };
+// person.age = person.age.toString();
+
+// let myString = JSON.stringify(person);
+// document.getElementById("demo").innerHTML = myString;
+
+// Strigify Array
+
+// const arr = ["John", "Peter", "Sally", "Jane"];
+
+// let myString = JSON.stringify(arr);
+// document.getElementById("demo").innerHTML = myString;
+
+
+
+// 5 *** Javascript Object Accessors ***
+
+// Javascript Accessors Getters and Setters
+
+// Javascript Get Keywords
+
+const person = {
+    fname: "Shamim",
+    lname: "Hasan",
+    language: "English",
+    get lang() {
+        return this.language;
+    }
+}
+
+document.getElementById("demo").innerHTML = person.lang;
