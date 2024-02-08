232a51863d3ba846b596fbf5bed7c25abd94c511
shamim496 <mdshamimhasan496@gmail.com>
2024-02-08 11:05:01 +0600

    prototype

--- js/object.js
+++ js/object.js
@@ -223,15 +223,117 @@
 
 // Javascript Accessors Getters and Setters
 
-// Javascript Get Keywords
+// Javascript Getter Keywords
 
-const person = {
-    fname: "Shamim",
-    lname: "Hasan",
-    language: "English",
-    get lang() {
-        return this.language;
-    }
-}
+// const person = {
+//     fname: "Shamim",
+//     lname: "Hasan",
+//     language: "English",
+//     get lang() {
+//         return this.language;
+//     }
+// }
 
-document.getElementById("demo").innerHTML = person.lang;
+// document.getElementById("demo").innerHTML = person.lang;
+
+
+//     const person = {
+//     fname: "Shamim",
+//     lname: "Hasan",
+//     language: "English",
+//     set lan(lang) {
+//         this.language = lang
+//     }
+// };
+
+// person.lan = "bn";
+
+// console.log(person);
+
+
+// Object.define property()
+
+// Define setters and getters
+
+// Object.defineProperty(obj, "reset", {
+    // get : function () {this.counter = 0;}
+// });
+
+
+// const person = {
+//     fname: "Shamim",
+//     lname: "Hasan",
+// };
+
+
+// Object.defineProperty(person, "fullName", {
+//     get : function () {
+//         return this.fname + " " + this.lname;
+//     }
+// });
+
+// console.log(person.fullName);
+
+
+
+// 6 *** Javascript Object Constructor ***
+
+// function Person(first, last, age, eye) { // Constructor function
+//     this.fname = first;
+//     this.lname = last;
+//     this.age = age;
+//     this.eyes = eye;
+//     this.fullName = function () {
+//         return this.fname + " " + this.lname;
+//     }
+//  };
+
+//  const shamim = new Person("Shamim", "Hasan", 35, "Black");
+//  const hamim = new Person("Shamim", "Hasan", "35");
+
+
+//  shamim.country = "Bangladesh";
+//  console.log(shamim);
+
+
+//  Built in Javascript Constructors
+
+// new String()    // A new String object
+// new Number()    // A new Number object
+// new Boolean()   // A new Boolean object
+// new Object()    // A new Object object
+// new Array()     // A new Array object
+// new RegExp()    // A new RegExp object
+// new Function()  // A new Function object
+// new Date()      // A new Date object
+
+// Math() is a global object
+
+
+
+// 7 *** Javascript Object Prototypes ***
+
+// Prototype inhiretance
+
+// function Person(first, last, age, eye) {
+//     this.fname = first;
+//     this.lname = last;
+//     this.age = age;
+//     this.eyes = eye;
+//     this.fullName = function () {
+//         return this.fname + " " + this.lname;
+//     }
+// };
+
+// const shamim = new Person("Shamim", "Hasan", 35, "Black");
+
+// Person.prototype.country = "Bangladesh";
+// console.dir(Person);
+
+// String.prototype.doingFun = function () {
+//     return 'I am Doing Fun';
+// }
+
+// const x = "";
+
+// console.log(x.doingFun())
