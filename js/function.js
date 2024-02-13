f30df7a57c287a391569a039cbbb3fcb6c1a8972
shamim496 <mdshamimhasan496@gmail.com>
2024-02-13 15:35:20 +0600

    function call

--- js/function.js
+++ js/function.js
@@ -179,3 +179,134 @@
 
 
 // 3 *** Javascript Function Invocation ***
+
+
+// invoking a javascript function
+
+// function myFunction(a, b) {
+//     return a * b;
+// }
+// myFunction(2, 3);
+
+// let a = 5;
+
+// function a() {
+//     console.log("I am a function")
+// }
+// console.log(window.a());
+
+// function a() {
+//     console.log(this);
+// }
+// a();
+
+// constructor function
+
+// This is constructor function
+// function myFunction(a, b) {
+//     this.fname = a;
+//     this.lanme = b;
+// }
+
+// This create a new object
+// const myObj = new myFunction("shamim", "hasan");
+
+// This will return "Shamim"
+// console.log(myObj.fname);
+
+
+
+// 4 *** Javascript Function Call ***
+
+// function myFunction(a, b) {
+//     this.fname = a;
+//     this.lname = b;
+// }
+
+// // call to function
+
+// myFunction(); // Invocation
+
+// new myFunction(); // Invocation // constructor function
+
+// myObject.myFunction(); // in
+
+
+// const a = {
+//     fname: "Shamim",
+//     sleep: function () {
+//         console.log(`${this.fname} is sleeping`);
+//     }
+// }
+
+// console.log(a.sleep());
+
+// function sleep () {
+//     console.log('i am sleeping');
+// }
+
+
+// const person = {
+//     fullName: function (city, country) {
+//         return this.firstName + " " + this.lastName + "" + city + " " + country;
+//     }
+// };
+// const person1 = {
+//     firstName: "John",
+//     lastName: "Doe"
+// };
+// const person2 = {
+//     firstName: "Mary",
+//     lastName: "Doe"
+// };
+
+// // This will return "John Doe":
+// const personAll = person.fullName.call(person1, "Dhaka", "Bangladesh");
+// console.log(personAll);
+
+
+
+// 5 *** Javascript Function Apply() Method ***
+
+// The apply() method is similar to the call() function
+
+// const person = {
+//     fullName: function (city, country) {
+//         return this.fname + " " + this.lname + " " + city + " " + country;
+//     }
+// }
+
+// const person1 = {
+//     fname: "Shamim",
+//     lname: "Hasan"
+// }
+
+// const person2 = {
+//     fname: "Asmaul",
+//     lname: "akter"
+// }
+
+// console.log(person.fullName.apply(person1, ["Bogura", "Bangladesh"]));
+
+
+//  Javascript Srrict mode
+
+
+
+// 6 *** Javascript Closures ***
+
+
+// function myFunction() {
+//     let a = 4; //local variables
+//     return a * a;
+// }
+
+
+// let a = 4; //global variables
+// function myFunction() {
+//     return a * a;
+// }
+
+// function myFunction() {
+//     a = 4; //global variables
+// }
