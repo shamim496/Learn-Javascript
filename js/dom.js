dab125955aa0884e0faed6180377157d77566eb3
shamim496 <mdshamimhasan496@gmail.com>
2024-02-20 11:59:06 +0600

    Dom Changing

--- js/dom.js
+++ js/dom.js
@@ -31,3 +31,95 @@
 
 // 3 *** Javascript HTML Dom Element ***
 
+// Finding HTML Elements By Tagname
+// const x = document.getElementsByTagName("p");
+
+// Finding HTML Elements By class name
+// const x = document.getElementsByClassName("intro");
+
+// Finding HTML Elements By CSS Selectors
+// const x = document.querySelectorAll("");
+
+// Finding HTML Elements By HTML Object Collections
+
+
+// document.anchors
+// document.body
+// document.documentElement
+// document.embeds
+// document.forms
+// document.head
+// document.images
+// document.links
+// document.scripts
+// document.title
+
+// const x = document.forms["frm1"];
+// let text = "";
+// for (let i = 0; i < x.length; i++) {
+//     text += x.elements[i].value + "<br>";
+// }
+// document.getElementById("demo").innerHTML = text;
+
+
+
+// 6 *** Javascript Dom Elements ***
+
+// Changing the value of an Attribute
+
+
+// const element = document.getElementById("myImage");
+
+// element.src = "test.jpg";
+
+// const date = document.getElementById("demo");
+
+// date.innerHTML = `Today's date is ${Date()}`;
+
+
+
+// 7 *** Javascript Dom Froms ***
+
+// Javascript From Validation
+
+// function ValidationFrom() {
+//     const from = document.forms["myForm"];
+//     const value = from['fname'].value;
+//     if (value === '') {
+//         alert("Type Your name");
+//         return false;
+//     }
+// }
+
+
+// function myFunction() {
+//     let x = document.getElementById("num").value;
+//     console.log(value);
+
+//     let text;
+
+//     if (isNaN(x) || x < 1 || x > 10) {
+//         text = "Input not valid";
+//     }else{
+//         text = "Input ok";
+//     }
+//     document.getElementById("demo").innerHTML = text;
+// }
+
+
+
+// 8 *** Javascript HTML DOM Changing CSS
+
+
+// const p = document.querySelector("#demo");
+
+// p.style.color = "red";
+// p.style.fontSize = "20px";
+
+
+function myStyle() {
+    const p = document.querySelector("#demo");
+
+    p.style.color = "red";
+    p.style.fontSize = "20px";
+}
