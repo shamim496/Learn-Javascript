a6ef09679a92d7a09d8b26ffe54218cb2629e636
shamim496 <mdshamimhasan496@gmail.com>
2024-04-01 12:47:11 +0600

    API

---
+++ js/API.js
@@ -0,0 +1,41 @@
+// 1 *** Javascript From Validation Api ***
+
+
+// function validation() {
+//     const inputObj = document.getElementById("id1");
+
+//     if (inputObj.validity.rangeOverflow) {
+//         inputObj.setCustomValidity("You have made a range overflow error");
+//     }else if(inputObj.validity.rangUnderflow){
+//         inputObj.setCustomValidity("you have range underflow error")
+//     }else if(inputObj.validity.valueMissing) {
+//         inputObj.setCustomValidity("you have range valueMissing error");
+//     }
+
+//     if (!inputObj.checkValidity()) {
+//         document.getElementById("demo").innerHTML = inputObj.validationMessage;
+//     }
+// }
+
+
+
+// 2 *** Javascript Web History Api ***
+
+// History Object Method
+// back(), forward(), go()
+
+// function back() {
+//     window.history.back();
+// }
+
+// function backTo() {
+//     window.history.go(n);
+// }
+
+// function forward() {
+//     window.history.forward();
+// }
+
+// function getHistory() {
+//     alert(window.history.length);
+// }
